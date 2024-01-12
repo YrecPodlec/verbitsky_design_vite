929490ac40e6ef176d920488a33737508dff2fbc
yrecpodlec <000333555666@mail.ru>
2024-01-12 17:36:12 +0300

    gallery

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'
+import envCompatible from "vite-plugin-env-compatible";
 dotenv.config()
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
+  plugins: [react(), envCompatible()],
+    resolve: {
     alias: {
       src: "/src"
     }
